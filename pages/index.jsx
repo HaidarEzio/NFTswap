74a6f40078a14092fe7c67ad14869d35b5ff8770
Haidar Ezio <haidar@haidarezio.me>
2022-06-17 21:01:54 +0100

    dead-end with the second account signing

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,11 @@
 import tw from "tailwind-styled-components";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-
 import { Input, Button, Loading } from "@nextui-org/react";
-
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-
 import { useConnect, useAccount, useDisconnect, chain } from "wagmi";
 import EthName from "../components/ETHName";
+import swap from "../utils/swapping";
 
 const Container = tw.div`
   flex
@@ -25,7 +23,7 @@ const Containing = tw.form`
   justify-between
   flex-col
   w-[25rem]
-  h-[30rem]
+  h-[40rem]
   py-5 rounded-2xl
   bg-red-400
   `;
@@ -34,12 +32,20 @@ export default function App() {
     initialValues: {
       nftHolder: "",
       nftContract: "",
+      myNFT: "",
     },
     validationSchema: Yup.object({
-      nftHolder: Yup.string().max(15, "Must be 15 characters or less").required("required"),
-      nftContract: Yup.string().max(15, "Must be 15 characters or less").required("required as well"),
+      nftHolder: Yup.string().min(42, "Must be 42 characters or less").required("required"),
+      nftContract: Yup.string().min(42, "Must be 42 characters or less").required("required as well"),
     }),
     onSubmit: (values) => {
+      //! the values are as follows
+      // {
+      //*  myNFT: "0x8Ec5fAC5fCB3e9B254d5BA06eF7F74569d0fba0A"
+      //*  nftContract: "0x5998AbEf6ac2105682f7799a75d4c23e423B6AbE";
+      //*  nftHolder: "0x4436C8962589a491D575f7531e7f8CB79A19aEBD";
+      // }
+      swap(account.address, values.myNFT, values.nftHolder, values.nftContract);
       console.log(values);
     },
   });
@@ -58,13 +64,27 @@ export default function App() {
         <div>
           <Input
             type="text"
+            name="myNFT"
+            id="myNFT"
+            {...formik.getFieldProps("myNFT")}
+            disabled={!account ? true : false}
+            clearable
+            size="lg"
+            labelPlaceholder="My NFT Contract"
+          />
+
+          {formik.touched.myNFT && formik.errors.myNFT ? <p>{formik.errors.myNFT}</p> : null}
+        </div>
+        <div>
+          <Input
+            type="text"
             name="NFTholder"
             id="NFTholder"
             {...formik.getFieldProps("nftHolder")}
             disabled={!account ? true : false}
             clearable
             size="lg"
-            placeholder="NFT holder"
+            labelPlaceholder="NFT holder"
           />
           {formik.touched.nftHolder && formik.errors.nftHolder ? <p>{formik.errors.nftHolder}</p> : null}
         </div>
@@ -77,11 +97,12 @@ export default function App() {
             disabled={!account ? true : false}
             clearable
             size="lg"
-            placeholder="NFT Contract"
+            labelPlaceholder="Other NFT Contract"
           />
 
           {formik.touched.nftContract && formik.errors.nftContract ? <p>{formik.errors.nftContract}</p> : null}
         </div>
+
         {!account ? (
           <Button
             css={{ background: "#df0d0de4" }}
@@ -94,7 +115,7 @@ export default function App() {
           <>
             <div>Connected to {account?.connector?.name}</div>
             <Button type="submit" css={{ background: "#f50b0b" }}>
-              Submit
+              Swap !
             </Button>
             <Button css={{ background: "#290ddfe3" }} onClick={disconnect}>
               Disconnect
